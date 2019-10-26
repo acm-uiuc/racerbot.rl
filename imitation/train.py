e34dc003fa66659e2926b8e9706af961549da681
kazuki-shin <kazukis2@illinois.edu>
2019-10-26 16:50:36 -0500

    training code

---
+++ imitation/train.py
@@ -0,0 +1,31 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 24, 5)
+        self.conv2 = nn.Conv2d(24, 36, 5)
+        self.conv3 = nn.Conv2d(36, 48, 5)
+        self.conv4 = nn.Conv2d(48, 64, 3)
+        self.conv5 = nn.Conv2d(64, 64, 3)
+        self.fc1 = nn.Linear(1164, 100)
+        self.fc2 = nn.Linear(100, 50)
+        self.fc3 = nn.Linear(50, 10)
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
+        x = self.pool(F.relu(self.conv4(x)))
+        x = self.pool(F.relu(self.conv5(x)))
+        x = x.view(-1, 1164)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+net = Net()
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+print('Finished Training')
